611b0d114e62c3ed9ca6d155890908e3339ddf67
p.prethiveerajan <prethiveerajan40@gmail.com>
2023-06-01 22:11:47 +0530

    update the whole code

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
-const express = require('express')
-const app = express ()
-const userRouter = require('./user')
-const service = require('./service')
-const about = require('./about')
+// const express = require('express')
+// const app = express ()
+// const userRouter = require('./user')
+// const service = require('./service')
+// const about = require('./about')
 
-app.get('/',(request,response)=>{
-    response.send('from app.js')
-    response.sendStatus(200)
-}
+// app.get('/',(request,response)=>{
+//     response.send('from app.js')
+//     response.sendStatus(200)
+// }
 
-)
-app.use('/user',userRouter)
-app.use('/service',service)
-app.use('/about',about)
+// )
+// app.use('/user',userRouter)
+// app.use('/service',service)
+// app.use('/about',about)
 
-app.listen(3400)
+// app.listen(3400)
+
+
+const express = require('express');
+const app = express();
+const userRouter = require('./user');
+const serviceRouter = require('./service');
+const aboutRouter = require('./about');
+
+app.get('/', (request, response) => {
+  response.send('from app.js');
+});
+
+app.use('/user', userRouter);
+app.use('/service', serviceRouter);
+app.use('/about', aboutRouter);
+
+app.listen(3400, () => {
+  console.log('Server is running on port 3400');
+});
+
--- service.js
+++ service.js
@@ -1,39 +1,76 @@
-const express = require('express')
-const userRouter = express.Router()
-const listofUser = [
-    {
-        'name' : 'gameDeveloper',
+// const express = require('express')
+// const userRouter = express.Router()
+// const listofUser = [
+//     {
+//         'name' : 'gameDeveloper',
         
-    },
-    {
-        'name' : 'webDesigner',
+//     },
+//     {
+//         'name' : 'webDesigner',
         
-    },
-    {
-        'name' : 'backendDeveloper',
+//     },
+//     {
+//         'name' : 'backendDeveloper',
         
-    },
-    {
-        'name' : 'FrontendDeveloper',
+//     },
+//     {
+//         'name' : 'FrontendDeveloper',
         
-    },
-    {
-        'name' : 'UiDesigner',
+//     },
+//     {
+//         'name' : 'UiDesigner',
         
-    }
-]
-userRouter.get('/',(response,request)=>{
-    response.send('this is user page')
-})
-userRouter.get('/:id',(request,response)=>{
-    response.send(request.user.name)
-})
-userRouter.param('id',(request,response,next,id)=>{
-    request.user = listofUser[id-1]
-    next()
+//     }
+// ]
+// userRouter.get('/',(response,request)=>{
+//     response.send('this is user page')
+// })
+// userRouter.get('/:id',(request,response)=>{
+//     response.send(request.user.name)
+// })
+// userRouter.param('id',(request,response,next,id)=>{
+//     request.user = listofUser[id-1]
+//     next()
 
 
-})
+// })
 
 
-module.exports   =  userRouter;
+// module.exports   =  userRouter;
+
+const express = require('express');
+const serviceRouter = express.Router();
+
+const listofServices = [
+  {
+    name: 'gameDeveloper',
+  },
+  {
+    name: 'webDesigner',
+  },
+  {
+    name: 'backendDeveloper',
+  },
+  {
+    name: 'FrontendDeveloper',
+  },
+  {
+    name: 'UiDesigner',
+  },
+];
+
+serviceRouter.get('/', (request, response) => {
+  response.send('This is the service page');
+});
+
+serviceRouter.get('/:id', (request, response) => {
+  const id = parseInt(request.params.id);
+  if (id > 0 && id <= listofServices.length) {
+    const serviceName = listofServices[id - 1].name;
+    response.send(serviceName);
+  } else {
+    response.status(404).send('Service not found');
+  }
+});
+
+module.exports = serviceRouter;
